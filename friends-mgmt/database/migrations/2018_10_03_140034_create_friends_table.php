6b66e14615c427815eba008d881983cbe70f7987
lvl100p <lvl100p@gmail.com>
2018-10-03 23:26:07 +0800

    Add migration for creating friends table

---
+++ friends-mgmt/database/migrations/2018_10_03_140034_create_friends_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user1_id');
+            $table->unsignedInteger('user2_id');
+            $table->timestamps();
+
+            $table->foreign('user1_id')
+                ->references('id')->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->foreign('user2_id')
+                ->references('id')->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
