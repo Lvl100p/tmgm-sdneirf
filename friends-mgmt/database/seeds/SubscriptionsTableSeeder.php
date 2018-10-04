ef511169aa2a9602934f5dc159aaabe9370e65af
lvl100p <lvl100p@gmail.com>
2018-10-04 21:09:42 +0800

    Add seeding for subscriptions table

---
+++ friends-mgmt/database/seeds/SubscriptionsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SubscriptionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('subscriptions')->insert([
+            'requestor_id' => 11,
+            'target_id' => 9,
+        ]);
+    }
+}
