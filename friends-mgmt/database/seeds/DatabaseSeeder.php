744a27a0445fa40dd5f75b87826a51dd2ee7ec12
lvl100p <lvl100p@gmail.com>
2018-10-05 17:03:04 +0800

    Add seeding for blocks table

--- friends-mgmt/database/seeds/DatabaseSeeder.php
+++ friends-mgmt/database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(FriendsTableSeeder::class);
         $this->call(SubscriptionsTableSeeder::class);
+        $this->call(BlocksTableSeeder::class);
     }
 }
