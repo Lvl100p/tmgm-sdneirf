9b6f541725ffd8c8a4825e19d684234c04e44cae
lvl100p <lvl100p@gmail.com>
2018-10-05 17:01:59 +0800

    Add migration for blocks table

---
+++ friends-mgmt/database/migrations/2018_10_05_085220_create_blocks_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blocks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('requestor_id');
+            $table->unsignedInteger('target_id');
+            $table->timestamps();
+
+            $table->foreign('requestor_id')
+                ->references('id')->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->foreign('target_id')
+                ->references('id')->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blocks');
+    }
+}
